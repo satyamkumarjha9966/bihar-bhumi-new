efbb9e7bcdb31750ff0a9c6e7fc1a15eee272ee6
satyamkumarjha9966 <satyamkumarjha9966@gmail.com>
2025-10-06 22:10:33 +0530

    icon update

--- src/components/CallBox.jsx
+++ src/components/CallBox.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "../styles/CallBox.css";
+import callIcon from "../assets/call.png";
 
 function CallBox() {
   return (
-    <div className="h-16 w-16 bg-white rounded-full shadow-lg flex justify-center items-center border-4 border-orange-600 animate-pulse-size">
-      <span className="material-symbols-outlined text-black text-3xl font-bold">
-        phone_in_talk
-      </span>
+    <div className="h-24 w-24 bg-white rounded-full shadow-lg flex justify-center items-center border-4 border-orange-600 animate-pulse-size">
+      <img src={callIcon} alt="" height={51} width={51} />
     </div>
   );
 }
